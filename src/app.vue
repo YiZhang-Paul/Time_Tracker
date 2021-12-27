1942705469cb0409e6da5e9b30d052d6e96face3
YI ZHANG <scaccterran@gmail.com>
2021-12-27 16:00:23 -0500

    TT-15 setup main background

--- src/app.vue
+++ src/app.vue
@@ -1,5 +1,5 @@
 <template>
-    <div></div>
+    <div class="container"></div>
 </template>
 
 <script lang="ts">
@@ -8,4 +8,24 @@ import { Vue } from 'vue-class-component';
 export default class App extends Vue { }
 </script>
 
-<style lang="scss"></style>
+<style lang="scss">
+@import './styles/presets.scss';
+
+html, body, #app {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+    width: 100vw;
+    height: 100vh;
+    user-select: none;
+}
+
+#app {
+    background-color: var(--primary-colors-10-00);
+}
+
+.container {
+    width: 100%;
+    height: 100%;
+}
+</style>
