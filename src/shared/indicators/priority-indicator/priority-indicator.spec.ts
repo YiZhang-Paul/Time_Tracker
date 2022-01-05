4f4dfb08edcf7c1783388ec54f34f475a3fd3834
YI ZHANG <scaccterran@gmail.com>
2022-01-04 21:52:59 -0500

    TT-29 priority indicator tests

---
+++ src/shared/indicators/priority-indicator/priority-indicator.spec.ts
@@ -0,0 +1,44 @@
+import { shallowMount, VueWrapper } from '@vue/test-utils';
+
+import { Priority } from '../../../core/enums/priority.enum';
+
+import PriorityIndicator from './priority-indicator.vue';
+
+describe('priority indicator unit test', () => {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    let component: VueWrapper<any>;
+
+    beforeEach(() => {
+        component = shallowMount(PriorityIndicator);
+    });
+
+    test('should create component instance', () => {
+        expect(component).toBeTruthy();
+    });
+
+    describe('color', () => {
+        test('should return correct color', async() => {
+            await component.setProps({ priority: Priority.Low });
+            expect(component.vm.color).toEqual('var(--priority-colors-low-0-00)');
+
+            await component.setProps({ priority: Priority.Medium });
+            expect(component.vm.color).toEqual('var(--priority-colors-medium-0-00)');
+
+            await component.setProps({ priority: Priority.High });
+            expect(component.vm.color).toEqual('var(--priority-colors-high-0-00)');
+        });
+    });
+
+    describe('icons', () => {
+        test('should return correct icons count', async() => {
+            await component.setProps({ priority: Priority.Low });
+            expect(component.vm.icons).toEqual(1);
+
+            await component.setProps({ priority: Priority.Medium });
+            expect(component.vm.icons).toEqual(2);
+
+            await component.setProps({ priority: Priority.High });
+            expect(component.vm.icons).toEqual(3);
+        });
+    });
+});
