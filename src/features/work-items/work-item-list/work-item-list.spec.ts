fd7f4533127e70e7c3fe032058d9eafa376de539
YI ZHANG <scaccterran@gmail.com>
2022-02-05 02:30:42 -0500

    TT-47 extracted item list base

--- src/features/work-items/work-item-list/work-item-list.spec.ts
+++ src/features/work-items/work-item-list/work-item-list.spec.ts
@@ -1,13 +1,23 @@
 import { shallowMount, VueWrapper } from '@vue/test-utils';
+import { createTestingPinia } from '@pinia/testing';
+
+import { useInterruptionStore } from '../../../stores/interruption/interruption.store';
+import { useTaskStore } from '../../../stores/task/task.store';
+import { InterruptionItem } from '../../../core/models/interruption/interruption-item';
+import { TaskItem } from '../../../core/models/task/task-item';
 
 import WorkItemList from './work-item-list.vue';
 
 describe('work item list unit test', () => {
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     let component: VueWrapper<any>;
+    let interruptionStore: ReturnType<typeof useInterruptionStore>;
+    let taskStore: ReturnType<typeof useTaskStore>;
 
     beforeEach(() => {
-        component = shallowMount(WorkItemList);
+        component = shallowMount(WorkItemList, { global: { plugins: [createTestingPinia()] } });
+        interruptionStore = useInterruptionStore();
+        taskStore = useTaskStore();
     });
 
     afterEach(() => {
@@ -16,5 +26,33 @@ describe('work item list unit test', () => {
 
     test('should create component instance', () => {
         expect(component).toBeTruthy();
+    });
+
+    describe('selectedInterruptionItemId', () => {
+        test('should return -1 when no item selected', () => {
+            interruptionStore.editingItem = null;
+
+            expect(component.vm.selectedInterruptionItemId).toEqual(-1);
+        });
+
+        test('should return id of selected item', () => {
+            interruptionStore.editingItem = new InterruptionItem(5);
+
+            expect(component.vm.selectedInterruptionItemId).toEqual(5);
+        });
+    });
+
+    describe('selectedTaskItemId', () => {
+        test('should return -1 when no item selected', () => {
+            taskStore.editingItem = null;
+
+            expect(component.vm.selectedTaskItemId).toEqual(-1);
+        });
+
+        test('should return id of selected item', () => {
+            taskStore.editingItem = new TaskItem(5);
+
+            expect(component.vm.selectedTaskItemId).toEqual(5);
+        });
     });
 });
--- src/features/work-items/work-item-list/work-item-list.vue
+++ src/features/work-items/work-item-list/work-item-list.vue
@@ -1,22 +1,36 @@
 <template>
     <div class="work-item-list-container">
-        <interruption-item-list class="interruption-item-list"
-            :searchText="searchText"
+        <item-list-base class="interruption-item-list"
+            :summaries="interruptionItemSummaries"
+            :activeSummary="interruptionStore.activeSummary"
+            :selectedId="selectedInterruptionItemId"
+            :type="eventType.Interruption"
+            :isRightToLeft="true"
             @select="$emit('select:interruption', $event)">
-        </interruption-item-list>
+        </item-list-base>
 
-        <task-item-list class="task-item-list"
-            :searchText="searchText"
+        <item-list-base class="task-item-list"
+            :summaries="taskItemSummaries"
+            :activeSummary="taskStore.activeSummary"
+            :selectedId="selectedTaskItemId"
+            :type="eventType.Task"
             @select="$emit('select:task', $event)">
-        </task-item-list>
+        </item-list-base>
     </div>
 </template>
 
 <script lang="ts">
 import { Options, Vue, prop } from 'vue-class-component';
+import { mapStores } from 'pinia';
 
-import InterruptionItemList from './interruption-item-list/interruption-item-list.vue';
-import TaskItemList from './task-item-list/task-item-list.vue';
+import { useInterruptionStore } from '../../../stores/interruption/interruption.store';
+import { useTaskStore } from '../../../stores/task/task.store';
+import { ItemSummariesDto } from '../../../core/dtos/item-summaries-dto';
+import { InterruptionItemSummaryDto } from '../../../core/dtos/interruption-item-summary-dto';
+import { TaskItemSummaryDto } from '../../../core/dtos/task-item-summary-dto';
+import { EventType } from '../../../core/enums/event-type.enum';
+
+import ItemListBase from './item-list-base/item-list-base.vue';
 
 class WorkItemListProp {
     public searchText = prop<string>({ default: '' });
@@ -24,15 +38,37 @@ class WorkItemListProp {
 
 @Options({
     components: {
-        InterruptionItemList,
-        TaskItemList
+        ItemListBase
     },
     emits: [
         'select:interruption',
         'select:task'
-    ]
+    ],
+    computed: {
+        ...mapStores(useInterruptionStore, useTaskStore)
+    }
 })
-export default class WorkItemList extends Vue.with(WorkItemListProp) { }
+export default class WorkItemList extends Vue.with(WorkItemListProp) {
+    public readonly eventType = EventType;
+    public interruptionStore!: ReturnType<typeof useInterruptionStore>;
+    public taskStore!: ReturnType<typeof useTaskStore>;
+
+    get interruptionItemSummaries(): ItemSummariesDto<InterruptionItemSummaryDto> {
+        return this.interruptionStore.filteredSummaries(this.searchText);
+    }
+
+    get taskItemSummaries(): ItemSummariesDto<TaskItemSummaryDto> {
+        return this.taskStore.filteredSummaries(this.searchText);
+    }
+
+    get selectedInterruptionItemId(): number {
+        return this.interruptionStore.editingItem?.id ?? -1;
+    }
+
+    get selectedTaskItemId(): number {
+        return this.taskStore.editingItem?.id ?? -1;
+    }
+}
 </script>
 
 <style lang="scss" scoped>
