91f933944dab9a5580e43725f7dbe3770b185abb
YI ZHANG <scaccterran@gmail.com>
2022-02-07 23:35:14 -0500

    TT-47 handle change event

--- src/features/work-items/work-item-editor/work-item-editor.vue
+++ src/features/work-items/work-item-editor/work-item-editor.vue
@@ -9,20 +9,22 @@
             class="item-editor"
             :item="interruptionStore.editingItem"
             :type="eventType.Interruption"
+            v-model:isSaved="isBaseSaved"
             @close="$emit('close:interruption', $event)"
             @create="$emit('create:interruption', $event)"
             @update="$emit('update:interruption', $event)"
+            @update:isSaved="$emit('update:isSaved', $event)"
             @delete="$emit('delete:interruption', $event)"
             @start="$emit('start:interruption', $event)"
             @stop="$emit('stop:interruption', $event)"
             @resolve="$emit('resolve:interruption', $event)"
             @unresolve="$emit('unresolve:interruption', $event)">
 
-            <template v-slot:footerActions>
+            <template v-slot:selector>
                 <selection-group class="priority-selector"
                     :options="priorityOptions"
                     :selectedOption="priority"
-                    @select="interruptionStore.editingItem.priority = $event.properties.priority">
+                    @select="onPrioritySelect($event)">
                 </selection-group>
             </template>
         </item-editor-base>
@@ -31,22 +33,22 @@
             class="item-editor"
             :item="taskStore.editingItem"
             :type="eventType.Task"
+            v-model:isSaved="isBaseSaved"
             @close="$emit('close:task', $event)"
             @create="$emit('create:task', $event)"
             @update="$emit('update:task', $event)"
+            @update:isSaved="$emit('update:isSaved', $event)"
             @delete="$emit('delete:task', $event)"
             @start="$emit('start:task', $event)"
             @stop="$emit('stop:task', $event)"
             @resolve="$emit('resolve:task', $event)"
             @unresolve="$emit('unresolve:task', $event)">
 
-            <template v-slot:footerActions>
+            <template v-slot:selector>
                 <selection-group class="effort-selector"
                     :options="effortOptions"
                     :selectedOption="taskStore.editingItem.effort"
-                    @select="taskStore.editingItem.effort = $event">
-
-                    <dumbbell class="icon" />
+                    @select="onEffortSelect($event)">
                 </selection-group>
             </template>
         </item-editor-base>
@@ -55,9 +57,8 @@
 
 <script lang="ts">
 import { markRaw } from '@vue/reactivity';
-import { Options, Vue } from 'vue-class-component';
+import { Options, Vue, prop } from 'vue-class-component';
 import { mapStores } from 'pinia';
-import { Dumbbell } from 'mdue';
 
 import { useInterruptionStore } from '../../../stores/interruption/interruption.store';
 import { useTaskStore } from '../../../stores/task/task.store';
@@ -69,9 +70,12 @@ import SelectionGroup from '../../../shared/inputs/selection-group/selection-gro
 
 import ItemEditorBase from './item-editor-base/item-editor-base.vue';
 
+class WorkItemEditorProp {
+    public isSaved = prop<boolean>({ default: true });
+}
+
 @Options({
     components: {
-        Dumbbell,
         PriorityIndicator,
         SelectionGroup,
         ItemEditorBase
@@ -83,6 +87,7 @@ import ItemEditorBase from './item-editor-base/item-editor-base.vue';
         'create:task',
         'update:interruption',
         'update:task',
+        'update:isSaved',
         'delete:interruption',
         'delete:task',
         'start:interruption',
@@ -98,11 +103,12 @@ import ItemEditorBase from './item-editor-base/item-editor-base.vue';
         ...mapStores(useInterruptionStore, useTaskStore)
     }
 })
-export default class WorkItemEditor extends Vue {
+export default class WorkItemEditor extends Vue.with(WorkItemEditorProp) {
     public readonly eventType = EventType;
     public readonly effortOptions = [1, 2, 3, 5, 8, 13];
     public interruptionStore!: ReturnType<typeof useInterruptionStore>;
     public taskStore!: ReturnType<typeof useTaskStore>;
+    public isBaseSaved = this.isSaved;
 
     public readonly priorityOptions = [Priority.Low, Priority.Medium, Priority.High].map(_ => {
         const component = markRaw(PriorityIndicator);
@@ -125,6 +131,16 @@ export default class WorkItemEditor extends Vue {
     get priority(): DynamicComponentOption<typeof PriorityIndicator> {
         return this.priorityOptions.find(_ => _.properties.priority === this.interruptionStore.editingItem!.priority)!;
     }
+
+    public onPrioritySelect(option: DynamicComponentOption<typeof PriorityIndicator>): void {
+        this.interruptionStore.editingItem!.priority = option.properties.priority as Priority;
+        this.$emit('update:isSaved', false);
+    }
+
+    public onEffortSelect(effort: number): void {
+        this.taskStore.editingItem!.effort = effort;
+        this.$emit('update:isSaved', false);
+    }
 }
 </script>
 
@@ -145,12 +161,18 @@ export default class WorkItemEditor extends Vue {
         @include animate-opacity(0, 1, 0.3s, 0.5s);
     }
 
-    .priority-selector {
-        font-size: var(--font-sizes-400);
+    .priority-selector, .effort-selector {
+        width: 100%;
+        height: 100%;
+        border-radius: 50%;
+        transition: background-color 0.3s, color 0.3s;
+
+        &:hover {
+            background-color: var(--primary-colors-7-00);
+        }
     }
 
     .effort-selector {
-        transition: color 0.3s;
 
         &:hover {
             color: var(--font-colors-0-00);
