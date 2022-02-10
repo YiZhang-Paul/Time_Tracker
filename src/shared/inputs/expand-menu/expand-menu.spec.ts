e222958cdbcb0a2d7be9a7770724aa17e324796e
YI ZHANG <scaccterran@gmail.com>
2022-02-09 21:40:03 -0500

    TT-47 added expand menu component

---
+++ src/shared/inputs/expand-menu/expand-menu.spec.ts
@@ -0,0 +1,20 @@
+import { shallowMount, VueWrapper } from '@vue/test-utils';
+
+import ExpandMenu from './expand-menu.vue';
+
+describe('expand menu unit test', () => {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    let component: VueWrapper<any>;
+
+    beforeEach(() => {
+        component = shallowMount(ExpandMenu);
+    });
+
+    afterEach(() => {
+        component.unmount();
+    });
+
+    test('should create component instance', () => {
+        expect(component).toBeTruthy();
+    });
+});
---
+++ src/shared/inputs/expand-menu/expand-menu.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="expand-menu-container"></div>
+</template>
+
+<script lang="ts">
+import { Options, Vue, prop } from 'vue-class-component';
+
+class ExpandMenuProp {
+    public options = prop<string[]>({ default: [] });
+}
+
+@Options({
+    emits: [
+        'select'
+    ]
+})
+export default class ExpandMenu extends Vue.with(ExpandMenuProp) { }
+</script>
+
+<style lang="scss" scoped>
+.expand-menu-container {
+
+}
+</style>
