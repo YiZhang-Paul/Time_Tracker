44d8accea3ca8ef95afa424152d48f01f04241b6
YI ZHANG <scaccterran@gmail.com>
2022-02-13 15:53:09 -0500

    TT-43 added progress indicator

--- src/shared/indicators/progress-indicator/progress-indicator.vue
+++ src/shared/indicators/progress-indicator/progress-indicator.vue
@@ -1,16 +1,29 @@
 <template>
-    <div class="progress-indicator-container"><slot></slot></div>
+    <div class="progress-indicator-container">
+        <div class="progress-wrapper">
+            <div class="progress" :style="progressStyles"></div>
+        </div>
+
+        <div class="content"><slot></slot></div>
+    </div>
 </template>
 
 <script lang="ts">
 import { Vue, prop } from 'vue-class-component';
+
+import { StyleConfigs } from '../../../core/models/generic/style-configs';
 
 class ProgressIndicatorProp {
     public progress = prop<number>({ default: 0 });
 }
 
 export default class ProgressIndicator extends Vue.with(ProgressIndicatorProp) {
-
+    get progressStyles(): StyleConfigs {
+        return {
+            height: `${this.progress}%`,
+            opacity: this.progress / 100 * 0.4
+        };
+    }
 }
 </script>
 
@@ -18,7 +31,7 @@ export default class ProgressIndicator extends Vue.with(ProgressIndicatorProp) {
 .progress-indicator-container {
     @import '../../../styles/presets.scss';
 
-    @include flex-row(center, center);
+    position: relative;
     width: 5vh;
     min-width: 5vh;
     height: 5vh;
@@ -29,5 +42,30 @@ export default class ProgressIndicator extends Vue.with(ProgressIndicatorProp) {
     color: var(--font-colors-2-00);
     font-size: var(--font-sizes-500);
     transition: background-color 0.4s;
+
+    .content, .progress-wrapper {
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        border-radius: 50%;
+    }
+
+    .content {
+        @include flex-row(center, center);
+    }
+
+    .progress-wrapper {
+        overflow: hidden;
+
+        .progress {
+            position: absolute;
+            bottom: 0;
+            width: 100%;
+            background-color: lightblue;
+            transition: height 0.5s, opacity 0.5s;
+        }
+    }
 }
 </style>
