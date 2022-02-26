00a43a37fedb1d4d0bc436c60fc544b8cdaa0005
YI ZHANG <scaccterran@gmail.com>
2022-02-26 00:26:18 -0500

    TT-64 added tooltip component

---
+++ src/shared/panels/tooltip-panel/tooltip-panel.spec.ts
@@ -0,0 +1,20 @@
+import { shallowMount, VueWrapper } from '@vue/test-utils';
+
+import TooltipPanel from './tooltip-panel.vue';
+
+describe('tooltip panel unit test', () => {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    let component: VueWrapper<any>;
+
+    beforeEach(() => {
+        component = shallowMount(TooltipPanel);
+    });
+
+    afterEach(() => {
+        component.unmount();
+    });
+
+    test('should create component instance', () => {
+        expect(component).toBeTruthy();
+    });
+});
---
+++ src/shared/panels/tooltip-panel/tooltip-panel.vue
@@ -0,0 +1,62 @@
+<template>
+    <div class="tooltip-panel-container">
+        <div class="tooltip-content" :class="position">{{ content }}</div>
+        <slot></slot>
+    </div>
+</template>
+
+<script lang="ts">
+import { Vue, prop } from 'vue-class-component';
+
+class TooltipPanelProp {
+    public content = prop<string>({ default: '' });
+    public position = prop<'top' | 'bottom' | 'left' | 'right'>({ default: 'top' });
+}
+
+export default class TooltipPanel extends Vue.with(TooltipPanelProp) { }
+</script>
+
+<style lang="scss" scoped>
+.tooltip-panel-container {
+    @import '../../../styles/presets.scss';
+    @import '../../../styles/animations.scss';
+
+    position: relative;
+
+    &:hover .tooltip-content {
+        display: block;
+    }
+
+    .tooltip-content {
+        $gap: 0.75vh;
+
+        z-index: 999;
+        display: none;
+        position: absolute;
+        padding: 0.5vh 1vh;
+        max-width: 25vw;
+        border-radius: 4px;
+        background-color: var(--context-colors-info-4-00);
+        color: var(--font-colors-0-00);
+        font-size: var(--font-sizes-300);
+        @include line-overflow();
+        @include animate-opacity(0, 1, 0.4s, 0.1s);
+
+        &.top {
+            bottom: calc(100% + #{$gap});
+        }
+
+        &.bottom {
+            top: calc(100% + #{$gap});
+        }
+
+        &.left {
+            right: calc(100% + #{$gap});
+        }
+
+        &.right {
+            left: calc(100% + #{$gap});
+        }
+    }
+}
+</style>
