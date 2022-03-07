86c0c597f981b0428004c51edec425929a04000c
YI ZHANG <scaccterran@gmail.com>
2022-03-06 19:24:09 -0500

    TT-67 allow searching and selecting work items

--- src/features/event-tracking/event-history/event-timeline-editor/event-selector/event-selector.vue
+++ src/features/event-tracking/event-history/event-timeline-editor/event-selector/event-selector.vue
@@ -1,6 +1,6 @@
 <template>
-    <div class="event-selector-container" :class="{ trigger: canOpenSelector }" @click="openSelector()">
-        <div v-if="!showSelector" class="selected">
+    <div class="event-selector-container" ref="container">
+        <div v-if="!showSelector" class="selected" :class="{ active: canOpenSelector }" @click="openSelector()">
             <component v-if="name"
                 class="icon"
                 :is="icon.component"
@@ -11,7 +11,23 @@
         </div>
 
         <div v-if="showSelector" class="selector">
-            <search-box class="search-box" :useCustomStyle="true"></search-box>
+            <search-box class="search-box"
+                :autoFocus="true"
+                :useCustomStyle="true"
+                @search="onSearch($event)">
+            </search-box>
+
+            <overlay-scrollbar-panel v-if="isSearchCompleted || searchResult.length" class="results">
+                <div v-for="(result, index) in searchResult"
+                    class="result"
+                    :key="index"
+                    @click="onResultSelect(result)">
+
+                    {{ result.name }}
+                </div>
+
+                <div v-if="!searchResult.length" class="result placeholder">no results.</div>
+            </overlay-scrollbar-panel>
         </div>
     </div>
 </template>
@@ -19,11 +35,19 @@
 <script lang="ts">
 import { Options, Vue, prop } from 'vue-class-component';
 
+import { types } from '../../../../../core/ioc/types';
+import { container } from '../../../../../core/ioc/container';
+import { InterruptionItemSummaryDto } from '../../../../../core/dtos/interruption-item-summary-dto';
+import { TaskItemSummaryDto } from '../../../../../core/dtos/task-item-summary-dto';
 import { IconConfig } from '../../../../../core/models/generic/icon-config';
 import { EventSelection } from '../../../../../core/models/event/event-selection';
 import { EventType } from '../../../../../core/enums/event-type.enum';
+import { DomUtility } from '../../../../../core/utilities/dom-utility/dom-utility';
 import { IconUtility } from '../../../../../core/utilities/icon-utility/icon-utility';
+import { InterruptionItemHttpService } from '../../../../../core/services/http/interruption-item-http/interruption-item-http.service';
+import { TaskItemHttpService } from '../../../../../core/services/http/task-item-http/task-item-http.service';
 import SearchBox from '../../../../../shared/inputs/search-box/search-box.vue';
+import OverlayScrollbarPanel from '../../../../../shared/panels/overlay-scrollbar-panel/overlay-scrollbar-panel.vue';
 
 class EventSelectorProp {
     public selected = prop<EventSelection>({ default: null });
@@ -31,7 +55,8 @@ class EventSelectorProp {
 
 @Options({
     components: {
-        SearchBox
+        SearchBox,
+        OverlayScrollbarPanel
     },
     emits: [
         'select'
@@ -39,6 +64,10 @@ class EventSelectorProp {
 })
 export default class EventSelector extends Vue.with(EventSelectorProp) {
     public showSelector = false;
+    public isSearchCompleted = false;
+    public searchResult: EventSelection[] = [];
+    private readonly interruptionItemHttpService = container.get<InterruptionItemHttpService>(types.InterruptionItemHttpService);
+    private readonly taskItemHttpService = container.get<TaskItemHttpService>(types.TaskItemHttpService);
 
     private readonly icons = {
         [EventType.Idling]: IconUtility.getIdlingTypeIcon(),
@@ -69,10 +98,53 @@ export default class EventSelector extends Vue.with(EventSelectorProp) {
         return this.selected.eventType === EventType.Idling ? 'Untracked' : 'Sleep & Break';
     }
 
+    public mounted(): void {
+        document.addEventListener('click', this.checkClickOutside);
+    }
+
+    public beforeUnmount(): void {
+        document.removeEventListener('click', this.checkClickOutside);
+    }
+
     public openSelector(): void {
         if (this.canOpenSelector) {
             this.showSelector = true;
         }
+    }
+
+    public async onSearch(searchText: string): Promise<void> {
+        if (!searchText?.trim()) {
+            return;
+        }
+
+        const { eventType } = this.selected;
+        let result: (InterruptionItemSummaryDto | TaskItemSummaryDto)[] = [];
+
+        if (eventType === EventType.Interruption) {
+            result = await this.interruptionItemHttpService.searchSummaries(searchText);
+        }
+        else if (eventType === EventType.Task) {
+            result = await this.taskItemHttpService.searchSummaries(searchText);
+        }
+
+        this.searchResult = result.map(_ => new EventSelection(_.id, eventType, _.name));
+        this.isSearchCompleted = true;
+    }
+
+    public onResultSelect(event: EventSelection): void {
+        this.$emit('select', event);
+        this.closeSelector();
+    }
+
+    private checkClickOutside(event: Event): void {
+        if (DomUtility.isClickOutside(event, this.$refs.container as HTMLElement)) {
+            this.closeSelector();
+        }
+    }
+
+    private closeSelector(): void {
+        this.showSelector = false;
+        this.isSearchCompleted = false;
     }
 }
 </script>
@@ -84,17 +156,20 @@ export default class EventSelector extends Vue.with(EventSelectorProp) {
 
     $selector-width: 15vw;
 
-    &.trigger:hover {
-        cursor: pointer;
+    @include flex-row(center);
 
-        .selected {
-            color: var(--context-colors-info-0-00);
-        }
+    .selected, .selector {
+        padding: 3px 10px;
     }
 
     .selected {
         @include flex-row(center);
         transition: color 0.3s;
+
+        &.active:hover {
+            cursor: pointer;
+            color: var(--context-colors-info-0-00);
+        }
 
         .icon {
             margin-right: 0.5vh;
@@ -108,13 +183,55 @@ export default class EventSelector extends Vue.with(EventSelectorProp) {
     }
 
     .selector {
-        @include animate-property(width, 5px, $selector-width, 0.5s);
+        $font-size: var(--font-sizes-300);
+
+        @include flex-column(center);
+        position: relative;
+        @include animate-property(width, 7.5vw, $selector-width, 0.5s);
 
         .search-box {
-            font-size: var(--font-sizes-300);
+            align-self: flex-start;
+            font-size: $font-size;
 
             &::v-deep(input) {
-                font-size: var(--font-sizes-300);
+                font-size: $font-size;
+            }
+        }
+
+        .results {
+            $border-radius: 4px;
+
+            position: absolute;
+            top: calc(100% + 0.75vh);
+            width: 95%;
+            max-height: 30vh;
+            border-radius: $border-radius;
+            box-shadow: 0 0 4px 1px rgba(0, 0, 0, 0.2);
+            background-color: var(--primary-colors-6-00);
+            font-size: $font-size;
+            @include animate-property(opacity, 0, 1, 0.2s, 0.5s);
+
+            .result {
+                padding: 0.5vh 1.25vh;
+                @include line-overflow();
+                scroll-snap-align: start;
+                transition: background-color 0.3s;
+                @include animate-property(opacity, 0, 1, 0.15s);
+
+                &:not(.placeholder):hover {
+                    cursor: pointer;
+                    background-color: var(--context-colors-info-4-00);
+                }
+
+                &:first-of-type {
+                    border-top-left-radius: $border-radius;
+                    border-top-right-radius: $border-radius;
+                }
+
+                &:last-of-type {
+                    border-bottom-left-radius: $border-radius;
+                    border-bottom-right-radius: $border-radius;
+                }
             }
         }
     }
