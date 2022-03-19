01419086bce2360401e8c072558631ba2d7a6e17
YI ZHANG <scaccterran@gmail.com>
2022-03-19 00:48:49 -0400

    TT-19 use show cases panel

--- src/features/login/login.vue
+++ src/features/login/login.vue
@@ -1,14 +1,21 @@
 <template>
     <div class="login-container">
         <div class="login-panel">
-
+            <show-case-panel class="show-case"></show-case-panel>
         </div>
     </div>
 </template>
 
 <script lang="ts">
-import { Vue } from 'vue-class-component';
+import { Options, Vue } from 'vue-class-component';
 
+import ShowCasePanel from './show-case-panel/show-case-panel.vue';
+
+@Options({
+    components: {
+        ShowCasePanel
+    }
+})
 export default class Login extends Vue { }
 </script>
 
@@ -21,11 +28,18 @@ export default class Login extends Vue { }
     z-index: 999;
 
     .login-panel {
+        @include flex-row();
         width: 65%;
         height: 87.5%;
         border-radius: 15px;
         box-shadow: 0 0 4px 1px rgba(0, 0, 0, 0.25);
+        overflow: hidden;
         @include animate-property(opacity, 0, 1, 0.75s);
+
+        .show-case {
+            width: 65%;
+            height: 100%;
+        }
     }
 }
 </style>
