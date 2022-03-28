3b60c10dfe44d34cf33d168057589e66f851f36c
YI ZHANG <scaccterran@gmail.com>
2022-03-27 20:38:59 -0400

    TT-19 try silent sign in on app start

--- src/router.ts
+++ src/router.ts
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router';
 
+import { useUserStore } from './stores/user/user.store';
 import Login from './features/login/login.vue';
 import ViewSelector from './features/view-selector/view-selector.vue';
 import WorkItems from './features/work-items/work-items.vue';
@@ -14,4 +15,20 @@ export const router = createRouter({
         { path: '/works', name: 'works', component: WorkItems },
         { path: '/histories', name: 'histories', component: EventHistory }
     ]
+});
+
+router.beforeEach(async to => {
+    const userStore = useUserStore();
+
+    if (to.name === 'login' || userStore.isLoggedIn) {
+        return true;
+    }
+
+    await userStore.silentSignIn();
+
+    if (!userStore.isLoggedIn) {
+        router.push('/login');
+
+        return false;
+    }
 });
